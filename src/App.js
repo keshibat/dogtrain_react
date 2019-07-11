1f20a6fc2ffafc579bf1b96f1cc915be3bd36de9
Nathaniel <nathanielmorales01@gmail.com>
2019-07-11 15:38:06 +1000

    add component did mount to check connection with express.js

--- src/App.js
+++ src/App.js
@@ -7,10 +7,17 @@ import AccomodationView from "./Pages/About/AccomodationView";
 import PetsittingView from "./Pages/About/PetsittingView";
 import ContactusView from "./Pages/About/ContactusView";
 
-
-
-
 class App extends React.Component {
+  componentDidMount() {
+    fetch("http://127.0.0.1:3000/")
+      .then(function(response) {
+        console.log(response);
+      })
+      .then(function(myJson) {
+        console.log(JSON.stringify(myJson));
+      });
+  }
+
   render() {
     return (
       <>
