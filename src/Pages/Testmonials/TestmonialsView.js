fdbad0b16a3b4c92b3f2f1238bb1269ef37a4473
Kensuke Shibata <kensukeshibata@gmail.com>
2019-07-12 02:09:20 +1000

    added all react route

---
+++ src/Pages/Testmonials/TestmonialsView.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class TestmonialsView extends Component {
+  render() {
+    return (
+      <>
+        <h1> Testmonials View</h1>
+      </>
+    );
+  }
+}
+
+export default TestmonialsView
