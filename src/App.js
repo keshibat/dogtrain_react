ade89ffc12d145e74ff3de6de43f412c29ca1f9d
Kensuke Shibata <kensukeshibata@gmail.com>
2019-07-12 00:27:05 +1000

    given router / and About section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "bulma/css/bulma.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import HomeView from "./Pages/HomeView";
 import AboutMe from "./Pages/About/AboutmeView";
 import AreasWeService from "./Pages/About/AreasweService";
 import FaqView from "./Pages/About/FaqView";
@@ -8,6 +10,7 @@ import PetsittingView from "./Pages/About/PetsittingView";
 import ContactusView from "./Pages/About/ContactusView";
 
 class App extends React.Component {
+
   componentDidMount() {
     fetch("http://127.0.0.1:3000/")
       .then(function(response) {
@@ -20,15 +23,19 @@ class App extends React.Component {
 
   render() {
     return (
-      <>
-        <h1>Hello World!</h1>
-        <AboutMe />
-        <AreasWeService />
-        <FaqView />
-        <AccomodationView />
-        <PetsittingView />
-        <ContactusView />
-      </>
+      <div>
+        <BrowserRouter>
+          <div>
+            <Route exact path="/" component={HomeView} />
+            <Route exact path="/aboutme" component={AboutMe} />
+            <Route exact path="/areasweservice" component={AreasWeService} />
+            <Route exact path="/faq" component={FaqView} />
+            <Route exact path="/accomodation" component={AccomodationView} />
+            <Route exact path="/petsitting" component={PetsittingView} />
+            <Route exact path="/contactus" component={ContactusView} />
+          </div>
+        </BrowserRouter>
+      </div>
     );
   }
 }
---
+++ src/Pages/HomeView.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class HomeView extends Component {
+  render() {
+    return (
+      <>
+        <h1> HOME Views </h1>
+      </>
+    );
+  }
+}
+
+export default HomeView
