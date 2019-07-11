703ae81a6fe953a082163afcd1164ae4b20770ca
Nathaniel <nathanielmorales01@gmail.com>
2019-07-12 09:04:49 +1000

    initial prac for bulma/ aboutmepage

--- src/Pages/About/AboutmeView.js
+++ src/Pages/About/AboutmeView.js
@@ -1,10 +1,37 @@
 import React, { Component } from "react";
+import MariaCunningHam from "./../../assets/images/maria-cunningham.jpg";
 
 export default class AboutmeView extends Component {
   render() {
     return (
       <>
         <h1> About Me View</h1>
+
+        <div class="container has-text-centered">
+          <div class="columns">
+            <div class="column is-half">
+              <div class="content" />
+              <img
+                class="is-centered"
+                src={MariaCunningHam}
+                alt="maria-cunningham"
+              />
+            </div>
+
+            <div class="column">
+              <div class="content is-centered">
+                <p>
+                  Mutts with Manners’ chief instructor, Maria Cunningham, has
+                  been a professional dog obedience trainer since 1993. This
+                  position of Assistant Trainer at Sydney Community College in
+                  Leichhardt entailed running the group dog training classes -
+                  some of the first group dog training classes to be
+                  professionally run in Sydney!
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </>
     );
   }
