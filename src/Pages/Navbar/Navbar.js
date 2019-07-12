69a6ed5d7fc5afd8f14be680b8f143f28de1e13f
Nathaniel <nathanielmorales01@gmail.com>
2019-07-12 11:35:59 +1000

    navbar file seperation

--- src/Pages/Navbar/Navbar.js
+++ src/Pages/Navbar/Navbar.js
@@ -1,46 +1,11 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import NavbarAboutUs from "./Navbar-AboutUs";
 
 export default class Navbar extends Component {
   render() {
     return (
       <nav class="navbar" role="navigation" aria-label="main navigation">
-        <div class="dropdown is-active">
-          <div class="dropdown-trigger">
-            <button
-              class="button"
-              aria-haspopup="true"
-              aria-controls="dropdown-menu"
-            >
-              <span>About Us</span>
-              <span class="icon is-small">
-                <i class="fas fa-angle-down" aria-hidden="true" />
-              </span>
-            </button>
-          </div>
-          <div class="dropdown-menu" id="dropdown-menu" role="menu">
-            <div class="dropdown-content">
-              <Link to="/aboutus">
-                <button>About Us</button>
-              </Link>
-              <Link to="/areasweservice">
-                <button>Areas We Service</button>
-              </Link>
-              <Link to="/petsitting">
-                <button>Pet Sitting Services</button>
-              </Link>
-              <Link to="/faq">
-                <button>Frequently Asked Questions</button>
-              </Link>
-              <Link to="/accomodation">
-                <button>Pet Friendly Accomodation</button>
-              </Link>
-              <Link to="/contactus">
-                <button>Contact Us</button>
-              </Link>
-            </div>
-          </div>
-        </div>
+        <NavbarAboutUs />
       </nav>
     );
   }
