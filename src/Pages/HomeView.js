18ce146e05d349768ce41ff1ca8e4a684a576912
Nathaniel <nathanielmorales01@gmail.com>
2019-07-12 15:57:28 +1000

    h1 title added to home page

--- src/Pages/HomeView.js
+++ src/Pages/HomeView.js
@@ -4,7 +4,15 @@ class HomeView extends Component {
   render() {
     return (
       <>
-        <h1> Expert Puppy Training, Dog Obedience & Dog Behaviour Training </h1>
+        <section class="section">
+          <div className="container has-text-centered">
+            <div className="content is-centered">
+              <h1 class="title">
+                Expert Puppy Training, Dog Obedience & Dog Behaviour Training
+              </h1>
+            </div>
+          </div>
+        </section>
       </>
     );
   }
