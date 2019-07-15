1a456f60f6cfdecddcf6283c244c00c9d28f7203
Nathaniel <nathanielmorales01@gmail.com>
2019-07-15 11:10:17 +1000

    s

--- src/Pages/About/AboutmeView.js
+++ src/Pages/About/AboutmeView.js
@@ -50,7 +50,7 @@ class AboutMeView extends Component {
                   <p>
                     Maria is also the author of "Ruling The Roost - What Your
                     Dog REALLY Wants You To Know!" available through our
-                    'Training Aids' page and also via Amazon as an e-book.
+                    'Training Aids' page and also via Amazon as an e-book.s
                   </p>
                 </div>
               </div>
