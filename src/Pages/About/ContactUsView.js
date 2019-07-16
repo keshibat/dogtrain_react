1ab48f7afe3dca49c12b263e35246da4faa75b34
Nathaniel <nathanielmorales01@gmail.com>
2019-07-16 17:10:47 +1000

    npm install axios/ implement form on ContactUsView to work with express

--- src/Pages/About/ContactUsView.js
+++ src/Pages/About/ContactUsView.js
@@ -1,9 +1,39 @@
 import React, { Component } from "react";
+import axios from "axios";
 import ContactUsCSS from "./../../assets/styles/AboutSCSS/ContactUsCSS.css";
 import ContactUsIMG from "./../../assets/images/contact-us.jpeg";
 
 class ContactUsView extends Component {
+  state = {
+    name: "",
+    email: "",
+    phone: "",
+    suburb: "",
+    dogDetails: "",
+    details: ""
+  };
+  onInputChange = (name, event) => {
+    this.setState({ [name]: event.target.value });
+  };
+  onFormSubmit = event => {
+    event.preventDefault();
+    const { name, email, phone, suburb, dogDetails, details } = this.state;
+    const newEnquiry = {
+      name,
+      email,
+      phone,
+      suburb,
+      dogDetails,
+      details
+    };
+    axios
+      .post("http://127.0.0.1:3000/contactus", newEnquiry)
+      .then(response => console.log(response))
+      .catch(err => console.log(err));
+  };
   render() {
+    const { name, email, phone, suburb, dogDetails, details } = this.state;
+
     return (
       <>
         <section className="section">
@@ -14,114 +44,136 @@ class ContactUsView extends Component {
           </div>
         </section>
 
-        <section className="section">
-          <div className="box content">
-            <div className="columns">
-              <div className="column">
-                <div className="field">
-                  <label className="label is-small">Name</label>
-                  <div className="control">
-                    <input
-                      className="input"
-                      type="text"
-                      placeholder="e.g Alex Smith"
-                      required
-                    />
+        <form onSubmit={this.onFormSubmit}>
+          <section className="section">
+            <div className="box content">
+              <div className="columns">
+                <div className="column">
+                  <div className="field">
+                    <label className="label is-small">Name</label>
+                    <div className="control">
+                      <input
+                        className="input"
+                        type="text"
+                        placeholder="e.g Alex Smith"
+                        required
+                        value={name}
+                        onChange={event => this.onInputChange("name", event)}
+                      />
+                    </div>
                   </div>
-                </div>
 
-                <div className="field">
-                  <label className="label is-small">Email</label>
-                  <div className="control">
-                    <input
-                      className="input"
-                      type="email"
-                      placeholder="e.g. alexsmith@gmail.com"
-                      required
-                    />
+                  <div className="field">
+                    <label className="label is-small">Email</label>
+                    <div className="control">
+                      <input
+                        className="input"
+                        type="email"
+                        placeholder="e.g. alexsmith@gmail.com"
+                        required
+                        value={email}
+                        onChange={event => this.onInputChange("email", event)}
+                      />
+                    </div>
                   </div>
-                </div>
-                <div className="field">
-                  <label className="label is-small">Phone</label>
-                  <div className="control">
-                    <input
-                      className="input"
-                      type="tel"
-                      placeholder="e.g 0400000000"
-                      required
-                    />
+                  <div className="field">
+                    <label className="label is-small">Phone</label>
+                    <div className="control">
+                      <input
+                        className="input"
+                        type="tel"
+                        placeholder="e.g 0400000000"
+                        required
+                        value={phone}
+                        onChange={event => this.onInputChange("phone", event)}
+                      />
+                    </div>
                   </div>
-                </div>
 
-                <div className="field">
-                  <label className="label is-small">Suburb</label>
-                  <div className="control">
-                    <input
-                      className="input"
-                      type="text"
-                      placeholder="e.g Camperdown"
-                      required
-                    />
+                  <div className="field">
+                    <label className="label is-small">Suburb</label>
+                    <div className="control">
+                      <input
+                        className="input"
+                        type="text"
+                        placeholder="e.g Camperdown"
+                        required
+                        value={suburb}
+                        onChange={event => this.onInputChange("suburb", event)}
+                      />
+                    </div>
                   </div>
-                </div>
-                <div className="field">
-                  <label className="label is-small">
-                    Name, Age, Breed of Dog
-                  </label>
-                  <div className="control">
-                    <input
-                      className="input"
-                      type="text"
-                      placeholder="Moki, 9 months, Shetland Sheepdog"
-                      required
-                    />
+                  <div className="field">
+                    <label className="label is-small">
+                      Name, Age, Breed of Dog
+                    </label>
+                    <div className="control">
+                      <input
+                        className="input"
+                        type="text"
+                        placeholder="Moki, 9 months, Shetland Sheepdog"
+                        required
+                        value={dogDetails}
+                        onChange={event =>
+                          this.onInputChange("dogDetails", event)
+                        }
+                      />
+                    </div>
                   </div>
-                </div>
 
-                <div className="field">
-                  <label className="label is-small">Details</label>
+                  <div className="field">
+                    <label className="label is-small">Details</label>
 
-                  <div className="field-body">
-                    <div className="field">
-                      <div className="control">
-                        <textarea
-                          className="textarea"
-                          placeholder="Any further details you would like to add?"
-                        />
+                    <div className="field-body">
+                      <div className="field">
+                        <div className="control">
+                          <textarea
+                            className="textarea"
+                            placeholder="Any further details you would like to add?"
+                            value={details}
+                            onChange={event =>
+                              this.onInputChange("details", event)
+                            }
+                          />
+                        </div>
                       </div>
                     </div>
                   </div>
+                  <div className="field">
+                    <div className="control has-text-centered">
+                      <input
+                        className="button is-link is-center"
+                        type="submit"
+                        value="Sumit"
+                      />
+                    </div>
+                  </div>
                 </div>
-                <div className="field">
-                  <div className="control has-text-centered">
-                    <button className="button is-link is-center">Submit</button>
+
+                <div className="column">
+                  <div className="container">
+                    <img
+                      className="is-centered dognoseimage"
+                      src={ContactUsIMG}
+                      alt="place-holder"
+                    />
+                  </div>
+                  <div className="container">
+                    <p>
+                      <b>Address:</b> PO Box 37 Camperdown NSW 1450
+                    </p>
+                    <p>
+                      <b>Email:</b> muttswithmanners1@gmail.com
+                    </p>
+                    <p>
+                      <b>Telephone:</b> 0410 626 677
+                    </p>
                   </div>
                 </div>
               </div>
-
-              <div className="column">
-                <div className="container">
-                  <img
-                    className="is-centered dognoseimage"
-                    src={ContactUsIMG}
-                    alt="place-holder"
-                  />
-                </div>
-                <div className="container">
-                  <p>
-                    <b>Address:</b> PO Box 37 Camperdown NSW 1450
-                  </p>
-                  <p>
-                    <b>Email:</b> muttswithmanners1@gmail.com
-                  </p>
-                  <p>
-                    <b>Telephone:</b> 0410 626 677
-                  </p>
-                </div>
-              </div>
             </div>
-          </div>
-        </section>
+          </section>
+        </form>
       </>
     );
   }
