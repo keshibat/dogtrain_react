649caccfa6e81b88208e64dbb9484d4982290904
Kensuke Shibata <kensukeshibata@gmail.com>
2019-07-17 11:10:11 +1000

    basic structure Trainning Section

--- src/Pages/Training/PuppyTrainingView.js
+++ src/Pages/Training/PuppyTrainingView.js
@@ -1,17 +1,19 @@
 import React, { Component } from "react";
-import puppy from "./../../assets/images/puppy.png";
+import mystyles from "./../../assets/styles/TrainningSCSS/mystyles.css";
+
 
 class PuppyTrainingView extends Component {
   render() {
     return (
       <>
-        <section class="hero is-link">
+        <section class="hero section">
           <div class="hero-body">
             <div class="container">
-              <h1 class="title">
+              <h1 class="title has-text-centered">
                 In-Home Puppy Training (from just 8 weeks of age!)
               </h1>
-              <h2 class="subtitle">
+              <br />
+              <h2 class="subtitle has-text-left">
                 Free follow-up email and telephone puppy training support available
                 from 48 hours after pup arrives home (this gives pup time to settle in)
                 all puppy issues and questions covered with our unique, gentle 'think dog' technique
@@ -21,90 +23,80 @@ class PuppyTrainingView extends Component {
           </div>
         </section>
 
-        <section>
-          <div class="columns">
-
-            <div class="column">
-              <article class="message is-primary">
-                <div class="message-header">
-                  <p class="title">Train your puppy early, and ENJOY him</p>
-                </div>
-                <div class="message-body">
-                  <div class="content is-primary">
-                    <figure class="image is-4by3">
-                      <img src={puppy} alt="puppy picture" />
-                    </figure>
-
+        <section class="has-background-light">
+            <div class="columns">
+              <div class="column">
+                <article1 class="message is-info">
+                  <div class="message-header">
+                    <p class="title has-text-centered">Train your puppy early, and ENJOY him</p>
                   </div>
-                </div>
-              </article>
-            </div>
-
-            <div class="column">
-              <article class="message is-primary">
-                <div class="message-header">
-                  <p class="title">A two hour puppy training consultation will include:</p>
-                </div>
-                <div class="message-body">
-                  <div class="content is-primary">
-                    <ul>
-                      <li class="subtitle">How to toilet train a puppy</li>
-                      <li class="subtitle">Stop play biting and nipping</li>
-                      <li class="subtitle">Appropriate games</li>
-                      <li class="subtitle">Setting rules and boundaries that work for ALL</li>
-                      <li class="subtitle">Chewing, mouthing, destructive behaviour, puppy aggression</li>
-                      <li class="subtitle">Whining, barking, crying and yelping (separation 'anxiety')</li>
-                    </ul>
+                  <div class="message-body">
+                    <div class="content">
+                      <figure class="image is-4by3">
+                        <img src="http://via.placeholder.com/640x360" />
+                      </figure>
+                    </div>
                   </div>
-                </div>
-              </article>
-            </div>
-
-            <div class="column">
-              <article class="message is-primary">
-                <div class="message-header">
-                  <p class="title">1 hour follow-up lesson with us to cover:</p>
-                </div>
-                <div class="message-body">
-                  <div class="content is-primary">
-                    <ul>
-                      <li class="subtitle">Pulling on the leash</li>
-                      <li class="subtitle">Barking at other dogs</li>
-                      <li class="subtitle">Come when called</li>
-                      <li class="subtitle">Appropriate play with other dogs</li>
-                      <li class="subtitle">Appropriate “Hello” with people too!</li>
-                    </ul>
+                </article1>
+              </div>
+              <div class="column">
+                <article1 class="message is-info">
+                  <div class="message-header">
+                    <p class="title has-text-centered">A two hour puppy training consultation will include:</p>
                   </div>
-                </div>
-              </article>
+                  <div class="message-body is-862x632.5">
+                    <div class="content is-primary">
+                      <ul>
+                        <li class="subtitle">How to toilet train a puppy</li>
+                        <li class="subtitle">Stop play biting and nipping</li>
+                        <li class="subtitle">Appropriate games</li>
+                        <li class="subtitle">Setting rules and boundaries that work for ALL</li>
+                        <li class="subtitle">Chewing, mouthing, destructive behaviour, puppy aggression</li>
+                        <li class="subtitle">Whining, barking, crying and yelping (separation 'anxiety')</li>
+                      </ul>
+                    </div>
+                  </div>
+                </article1>
+              </div>
+              <div class="column">
+                <article1 class="message is-info">
+                  <div class="message-header">
+                    <p class="title has-text-centered">1 hour follow-up lesson with us to cover:</p>
+                  </div>
+                  <div class="message-body">
+                    <div class="content is-primary">
+                      <ul>
+                        <li class="subtitle">Pulling on the leash</li>
+                        <li class="subtitle">Barking at other dogs</li>
+                        <li class="subtitle">Come when called</li>
+                        <li class="subtitle">Appropriate play with other dogs</li>
+                        <li class="subtitle">Appropriate “Hello” with people too!</li>
+                      </ul>
+                    </div>
+                  </div>
+                </article1>
+              </div>
             </div>
-
-          </div>
         </section>
 
-        <section class="hero is-link">
+        <section class="hero">
           <div class="hero-body">
             <div class="container">
-              <h1 class="title">
+              <h1 class="title has-text-centered">
                 Resolve toilet training, play biting and puppy separation anxiety quickly!
               </h1>
-              <h2 class="subtitle">
+              <br />
+              <h2 class="subtitle has-text-centered">
                 Puppy training will also cover any other puppy problems that may have come up.
               </h2>
             </div>
           </div>
         </section>
 
-
-        <section>
+        <section class="quotes" >
           <div class="columns">
-
             <div class="column">
-              <article class="message">
-                <div class="message-header">
-                  <p>Julie, Brent & Misty</p>
-
-                </div>
+              <article class="message is-primary is-medium">
                 <div class="message-body">
                   We made various attempts to take Indie to puppy training school
                   where the classes were more like a battle with our dog
@@ -112,57 +104,55 @@ class PuppyTrainingView extends Component {
                   Then we were very happy to meet you and join together to train our beautiful new pup.
                   Your advice and assistance in training was comparable to none and the difference in the behaviour of Indie was noticeable in 2 weeks.
                   We look forward to seeing you soon. Thanks again.
-        </div>
+                  <br />
+                  <strong>Sam, Mike & Indie</strong>
+                </div>
               </article>
             </div>
 
-
-
             <div class="column">
-              <article class="message">
-                <div class="message-header">
-                  <p>Julie, Brent & Misty</p>
-
-                </div>
+              <article class="message is-info is-medium">
                 <div class="message-body">
                   I would like to start with a big Thank You. Misty and I really enjoyed puppy training.
                     We learnt so much and lots of great tips.
                     What I liked is I felt I could talk to you and ask you anything
-                    and you had great advice to help us through. Thanks so much,
-        </div>
+                    and you had great advice to help us through.
+                    <br />
+                    Thanks so much, <strong>Julie, Brent & Misty</strong>
+                </div>
               </article>
             </div>
 
-
-
-
-
           </div>
         </section>
-        <section class="hero is-danger">
+
+        <section class="hero has-background-light">
           <div class="hero-body">
             <div class="container">
-              <h1 class="title">
+              <h1 class="title has-text-centered">
                 Missed Appointment and Cancellation Policy
               </h1>
-              <h2 class="subtitle">
+              <br />
+              <h2 class="subtitle has-text-left">
                 If you are unable to keep an appointment
                 please give at least 24 hours notice to ensure that
                 you will not be charge for the appointment or part-thereof.
               </h2>
-              <h2 class="subtitle">
+              <h2 class="subtitle has-text-left">
                 12 to 24 hours notice of cancellation will require $50 value to be paid,
                 less than 12 hours notice will require full payment.
               </h2>
-              <h2 class="subtitle">
+              <h2 class="subtitle has-text-left">
                 Thank you for your consideration in ensuring that adequate notice is given.
               </h2>
             </div>
           </div>
         </section>
+
       </>
     );
   }
 }
+
 
 export default PuppyTrainingView;
