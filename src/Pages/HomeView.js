ef3a7c5664508664d3eacf57efd1b1076eeb8c24
Nathaniel <nathanielmorales01@gmail.com>
2019-07-18 15:49:22 +1000

    implement twitter feed into HomeView.js

--- src/Pages/HomeView.js
+++ src/Pages/HomeView.js
@@ -1,15 +1,57 @@
 import React, { Component } from "react";
 
 class HomeView extends Component {
+  componentDidMount() {
+    window.twttr.widgets.load();
+  }
+
   render() {
     return (
       <>
         <section className="section title-heading">
-          <div className="container has-text-centered">
-            <div className="content is-centered">
-              <h1 className="title">
-                Expert Puppy Training, Dog Obedience & Dog Behaviour Training
-              </h1>
+          <div className="container">
+            <div className="content has-text-centered">
+              <h1>Mutts with Manners</h1>
+            </div>
+          </div>
+        </section>
+        <section className="section title-heading">
+          <div className="container">
+            <div className="content">
+              <div className="columns">
+                <div className="column">
+                  <a
+                    className="twitter-timeline"
+                    data-theme="light"
+                    href="https://twitter.com/MuttswithMannrs?ref_src=twsrc%5Etfw"
+                    data-width="300"
+                    data-height="300"
+                  >
+                    Tweets by MuttswithMannrs
+                  </a>
+                  <script
+                    async
+                    src="https://platform.twitter.com/widgets.js"
+                    charSet="utf-8"
+                  />
+                </div>
+                <div className="column">
+                  <a
+                    className="twitter-timeline"
+                    data-theme="light"
+                    href="https://twitter.com/MuttswithMannrs?ref_src=twsrc%5Etfw"
+                    data-width="300"
+                    data-height="300"
+                  >
+                    Tweets by MuttswithMannrs
+                  </a>
+                  <script
+                    async
+                    src="https://platform.twitter.com/widgets.js"
+                    charSet="utf-8"
+                  />
+                </div>
+              </div>
             </div>
           </div>
         </section>
