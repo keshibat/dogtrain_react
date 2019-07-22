73141ae110003a07be38077a2a7c8be7011e44a7
John <johnrubio93@gmail.com>
2019-07-22 10:05:48 +1000

    merged with nathaniel and fixed conflicts

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "bulma/css/bulma.css";
 import { BrowserRouter, Route } from "react-router-dom";
 import LocalAPI from "./apis/local";
 import PrivateRoute from "./components/PrivateRoute";
-import HomeView from "./Pages/HomeView";
+import HomeView from "./Pages/Home/HomeView";
 import AboutMeView from "./Pages/About/AboutMeView";
 import AreasWeServiceView from "./Pages/About/AreasWeServiceView";
 import FaqView from "./Pages/About/FaqView";
