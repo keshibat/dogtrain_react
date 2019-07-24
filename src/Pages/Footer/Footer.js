6a07852cfc16bbd64b57102bd97039a91625e6f8
Nathaniel <nathanielmorales01@gmail.com>
2019-07-24 14:00:32 +1000

    fix errors in footer, naming convention and attribute

--- src/Pages/Footer/Footer.js
+++ src/Pages/Footer/Footer.js
@@ -11,13 +11,13 @@ class Footer extends Component {
             <strong>CONTACT US</strong> <span>0410 626 677</span>
           </h5>
           <h5 className="has-text-centered">
-            <strong has-text-centered>FIND US</strong>{" "}
+            <strong className="has-text-centered">FIND US</strong>{" "}
             <span>PO Box 37 Camperdown NSW 1450</span>
           </h5>
         </div>
         <div>
           <div className="columns has-text-centered is-size-1 is-gapless">
-            <div class="column">
+            <div className="column">
               <div className="footericons">
                 <FontAwesomeIcon icon={["fab", "instagram"]} />
 
